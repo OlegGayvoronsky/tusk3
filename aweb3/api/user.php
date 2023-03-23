ffa50e4b8f6a183fae761d79e77c2942472b4736
OlegGayvoronsky <113290639+OlegGayvoronsky@users.noreply.github.com>
2023-03-23 10:51:28 +0300

    Update user.php

--- aweb3/api/user.php
+++ aweb3/api/user.php
@@ -7,7 +7,7 @@ use Controller\PersonController;
 use System\DatabaseConnector;
 
 
-header("Access-Control-Allow-Origin: http://frontend.aweb3.ayarayarovich.tech");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
