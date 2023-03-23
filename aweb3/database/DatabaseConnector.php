633231d8a36106acea477b56f70a0fec052b7e8b
OlegGayvoronsky <113290639+OlegGayvoronsky@users.noreply.github.com>
2023-03-23 09:55:21 +0300

    Add files via upload

---
+++ aweb3/database/DatabaseConnector.php
@@ -0,0 +1,32 @@
+<?php
+namespace System;
+use PDO;
+use PDOException;
+
+class DatabaseConnector {
+    private $m_dbConnection = null;
+
+    public function __construct() {
+        $host = 'localhost';
+        $port = '3306';
+        $db = 'u52831';
+        $user = 'u52831';
+        $password = '2026122';
+
+        try {
+            $this->m_dbConnection = new PDO(
+                "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
+                $user,
+                $password
+            );
+        }
+        catch (PDOException $exception) {
+            exit($exception->getMessage());
+        }
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->m_dbConnection;
+    }
+}
